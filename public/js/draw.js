a1ec8de3dc33fff384f4bf31a42eb1310c9bb3b8
tommy96 <tomomo96@gmail.com>
2016-05-10 12:30:35 +0900

    色と太さが変更可能に！！！

--- public/js/draw.js
+++ public/js/draw.js
@@ -13,8 +13,31 @@ $(function () {
   context2d.fillStyle = '#FFF';
   context2d.fillRect(0, 0, width, height);
 
+  var randomColor = [
+    '#ff7f7f',
+    '#ff7fbf',
+    '#ff7fff',
+    '#bf7fff',
+    '#7f7fff',
+    '#7fbfff',
+    '#7fffff',
+    '#7fffbf',
+    '#7fff7f',
+    '#bfff7f',
+    '#ffff7f',
+    '#ffbf7f'
+  ];
+
   // マウスを押し始めた時
   $canvas.mousedown(function (e) {
+    if($('#randomMode').prop('checked')){
+      context2d.lineWidth = Math.floor( Math.random() * 100 );
+      context2d.strokeStyle = randomColor[ Math.floor( Math.random() * randomColor.length ) ] ;
+    }else{
+      context2d.lineWidth = $('#lineWidth').val();
+      context2d.strokeStyle = $('#lineColor').val();
+    }
+
     var x = e.originalEvent.layerX; // 行き先
     var y = e.originalEvent.layerY; // 行き先
 
@@ -25,6 +48,7 @@ $(function () {
 
   // マウスを動かしているあいだ中
   $canvas.mousemove(function (e) {
+
     var x = e.originalEvent.layerX; // 行き先
     var y = e.originalEvent.layerY; // 行き先
     if (isDrawing) {
@@ -42,6 +66,40 @@ $(function () {
   $canvas.mouseleave(function (e) {
     isDrawing = false;
   });
+
+  //削除
+  $("button#allDelete").on("click",function() {
+      context2d.fillStyle = '#FFF';
+      context2d.fillRect(0, 0, width, height);
+  });
+
+  //redo, undo
+  var cPushArray = new Array();
+  var cStep = -1;
+
+  function cPush() {
+      cStep++;
+      if (cStep < cPushArray.length) { cPushArray.length = cStep; }
+      cPushArray.push(document.canvas.toDataURL());
+  }
+
+  function cUndo() {
+    if (cStep > 0) {
+        cStep--;
+        var canvasPic = new Image();
+        canvasPic.src = cPushArray[cStep];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
+  }
+
+  function cRedo() {
+    if (cStep < cPushArray.length-1) {
+        cStep++;
+        var canvasPic = new Image();
+        canvasPic.src = cPushArray[cStep];
+        canvasPic.onload = function () { ctx.drawImage(canvasPic, 0, 0); }
+    }
+  }
 
   // 保存
   $('button.save').click(function (e) {
